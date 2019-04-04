a42f339880bac90987278162bd09f4514babb4bc
Angela <angela@Angelas-MacBook-Pro.local>
2019-04-04 16:32:26 -0700

    add sign up styling

--- src/modules/signup/signup.js
+++ src/modules/signup/signup.js
@@ -6,16 +6,29 @@ const accountImage = require("../../assets/account.png");
 class SignUp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showPassword: false
+        }
+        this.togglePassword = this.togglePassword.bind(this);
     }
+
+    togglePassword() {
+        this.setState({
+            showPassword: !this.state.showPassword
+        });
+    }
+
     render() {
         const { changePage } = this.props;
+        const { showPassword } = this.state;
+        const iconClass = showPassword ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-eye-close";
         return (
             <div>
                 <div id="entireSignUpBox">
                     <div id="fillInTheBlank">
                         <div id="googlelogo">
                             <img 
-                                id="googlelogo"
+                                id="imageLogo"
                                 src={googlelogo}
                             />
                         </div>
@@ -25,75 +38,87 @@ class SignUp extends Component {
                         <div id="toContinueToGmail">
                             to continue to Gmail
                         </div>
-                        <div id="nameInput">
-                            <input 
-                                id="firstNameInput"
-                                placeholder="First name"
-                            />
-                            <input 
-                                id="lastNameInput"
-                                placeholder="Last name"
-                            />
-                        </div>
-                        <div id="username">
-                            <input 
-                                id="usernameInput"
-                                placeholder="Username"
-                            />
-                            <div id="endGmailCom">
-                                @gmail.com
+                        <div id="allInputs">
+                            <div id="nameInput">
+                                <input 
+                                    id="firstNameInput"
+                                    placeholder="First name"
+                                />
+                                <input 
+                                    id="lastNameInput"
+                                    placeholder="Last name"
+                                />
                             </div>
-                        </div>
-                        <div id="userNameInsturction">
-                        You can use letters, numbers & periods
-                        </div>
-                        <div id="pwpw">
-                            <input
-                                id="pw"
-                                placeholder="Password"
-                                type="password"
-                            />
-                            <input
-                            id="passwordConfirm"
-                            placeholder="Confirm"
-                            type="password"
-                            />
-                        </div>
-                        <div id="pwInstruction">
-                        Use 8 or more characters with a mix of letters, numbers & symbols
-                        </div>
-                        <div id="signInAndNext">
-                            <div onClick={() => changePage("SignIn")} id="signInInstead">
-                            Sign in instead
+                            <div id="username">
+                                <input 
+                                    id="usernameInput"
+                                    placeholder="Username"
+                                />
+                                <div id="endGmailContainer">
+                                    <div id="gmailDotCom">
+                                        @gmail.com
+                                    </div>
+                                </div>
                             </div>
-                            <div onClick={() => changePage("EmailList")} id="next">
-                            Next
+                            <div id="userNameInstruction">
+                            You can use letters, numbers & periods
+                            </div>
+                            <div id="pwpw">
+                                <input
+                                    id="pw"
+                                    placeholder="Password"
+                                    type={showPassword ? "text" : "password"}
+                                />
+                                <input
+                                    id="passwordConfirm"
+                                    placeholder="Confirm"
+                                    type={showPassword ? "text" : "password"}
+                                />
+                                <div 
+                                    id="eyeball" 
+                                    className={iconClass}
+                                    onClick={this.togglePassword}
+                                />
+                            </div>
+                            <div id="pwInstruction">
+                            Use 8 or more characters with a mix of letters, numbers & symbols
+                            </div>
+                            <div id="signInAndNext">
+                                <div onClick={() => changePage("SignIn")} id="signInInstead">
+                                Sign in instead
+                                </div>
+                                <div onClick={() => changePage("EmailList")} id="next">
+                                Next
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div id="rightBox">
-                        <div id="imageLogo">
-                            <img 
-                                id="accountImage"
-                                src={accountImage}
-                            />
-                        </div>
-                        <div id="explanationUnderImage">
-                        One account. All of Google working for you.
+                        <div id="imageContainer">
+                            <div id="imageLogo">
+                                <img 
+                                    id="accountImage"
+                                    src={accountImage}
+                                />
+                            </div>
+                            <div id="explanationUnderImage">
+                            One account. All of Google working for you.
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div id="signUpBoxHelp">
-                    <div id="one">English (United States)
-                    <select>
-                    <option value="korean">Korean</option>
-                    <option value="spanish">Spanish</option>
-                    <option value="french">French</option>
-                    <option value="English">English</option>
-                    </select></div>
-                    <div id="one">Help</div>
-                    <div id="one">Privacy</div>
-                    <div id="one">Terms</div>
+                    <select id="languageBox">
+                        <option value="korean">Korean</option>
+                        <option value="spanish">Spanish</option>
+                        <option value="french">French</option>
+                        <option selected value="English">English (United States)</option>
+                    </select>
+                    <div id="signUpHelpRight">
+                        <div id="one">Help</div>
+                        <div id="one">Privacy</div>
+                        <div id="one">Terms</div>
+                    </div>
                 </div>
             </div>
         )
--- src/reducers/utils.js
+++ src/reducers/utils.js
@@ -1,6 +1,6 @@
 export const generateEmail = (email) => {
-    const nouns = ["angela", "andrew", "olivia", "gaji", "mandoo"];
-    const verbs = ["farts", "poops", "pees"];
+    const nouns = ["angela", "gaji", "mandoo"];
+    const verbs = ["smiles", "dances", "laughs"];
     email.senderEmail = "no-reply@accounts.google.com";
     email.body = `The password for your Google account ${pickRandomFromArr(nouns)}${pickRandomFromArr(verbs)}247@gmail.com was changed. If you didn't change it, you should recover your account.`
     return email;
