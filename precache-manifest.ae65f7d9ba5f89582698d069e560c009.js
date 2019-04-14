49ba69cbbef36b0889509bd81e77d04553473776
Angela <angela@angelas-mbp.attlocal.net>
2019-04-14 14:03:17 -0700

    Updates

---
+++ precache-manifest.ae65f7d9ba5f89582698d069e560c009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aca515379dc5af1057e9a549236af76",
+    "url": "/Gmail/static/media/account.1aca5153.png"
+  },
+  {
+    "revision": "6584ec4a42bdf24fd4fa",
+    "url": "/Gmail/static/js/runtime~main.9624231e.js"
+  },
+  {
+    "revision": "d0dabd702dad751d001c",
+    "url": "/Gmail/static/js/main.98c204db.chunk.js"
+  },
+  {
+    "revision": "1891dc67e18de4f48256",
+    "url": "/Gmail/static/js/2.95761956.chunk.js"
+  },
+  {
+    "revision": "d0dabd702dad751d001c",
+    "url": "/Gmail/static/css/main.85c3e76a.chunk.css"
+  },
+  {
+    "revision": "afeb092e378c714ac189efa2ca7e6e52",
+    "url": "/Gmail/index.html"
+  }
+];
